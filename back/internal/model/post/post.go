55eb144df38ba17e0977a6f3d915963639b6245a
ArshiA Akhavan <letmemakenewone@gmail.com>
2020-12-25 15:10:08 +0330

    adding models and their curresponding methods

--- back/internal/model/post/post.go
+++ back/internal/model/post/post.go
@@ -1,1 +1,29 @@
 package post
+
+import (
+	"time"
+	"yes-blog/graph/model"
+)
+
+type Post struct {
+	Title     string
+	Body      string
+	Author    *model.User
+	TimeStamp time.Time
+}
+
+func NewPost(title, body string, author *model.User) *Post {
+	return &Post{
+		Title:     title,
+		Body:      body,
+		Author:    author,
+		TimeStamp: time.Now(),
+	}
+}
+
+func (p *Post) EditTitle(title string) {
+	p.Title = title
+}
+func (p *Post) EditBody(body string) {
+	p.Body = body
+}
--- back/internal/model/user/user.go
+++ back/internal/model/user/user.go
@@ -1,1 +1,39 @@
 package user
+
+type User struct {
+	Name     string
+	password string
+	admin    bool
+}
+
+func NewUser(name,password string) *User{
+	return newUser(name, password,false)
+}
+
+func NewAdmin(name,password string) *User{
+	return newUser(name, password,true)
+}
+
+func newUser(name,password string , admin bool) *User{
+	return &User{
+		Name: name,
+		password: password,
+		admin: admin,
+	}
+}
+
+func (u *User)UpdatePassword(password string) {
+	u.password=password
+}
+
+func (u *User)UpdateName(name string) {
+	u.Name=name
+}
+
+func (u *User)Upgrade() {
+	u.admin=true
+}
+
+func (u *User)degrade() {
+	u.admin=false
+}
