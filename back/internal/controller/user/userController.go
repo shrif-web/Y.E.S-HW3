f147225aa5a13a90391d5ef092ca3fd5121a7ae3
ArshiA Akhavan <letmemakenewone@gmail.com>
2020-12-27 10:02:43 +0330

    removing excess imports

--- back/internal/controller/user/userController.go
+++ back/internal/controller/user/userController.go
@@ -1,10 +1,7 @@
 package controller
 
 import (
-	"errors"
-	"yes-blog/graph/model"
 	"yes-blog/pkg/database"
-	"yes-blog/pkg/database/status"
 )
 
 type UserController struct {
@@ -16,9 +13,6 @@ var userC *UserController
 func (c *UserController) SetDBDriver(dbDriver database.UserDBDriver) {
 	userC.dbDriver = dbDriver
 }
-
-
-
 
 func init() {
 	userC = &UserController{}
