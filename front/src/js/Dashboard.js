57386b219b7f45add46b6353371b39d2390a2071
baharkhd <baharkh127@gmail.com>
2021-01-05 20:49:47 +0330

    merge with front

--- front/src/js/Dashboard.js
+++ front/src/js/Dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
+import constants from "../constants";
 
 const GET_USER_POSTS = gql`
   query Posts($username: String!) {
-    postsOfUser(username: $username) {
+    postsOfUser(userName: $username) {
       id
       title
       content
@@ -18,11 +19,16 @@ const GET_USER_POSTS = gql`
 
 const UserPosts = props => {
 
-//   const { data, loading, error } = useQuery(GET_USER_POSTS, {
-//       variables: {
-//           username: 
-//       }
-//   });
+  const { data, loading, error } = useQuery(GET_USER_POSTS, {
+      variables: {
+          username: localStorage.getItem(constants.AUTH_TOKEN)
+      }
+  });
+
+  console.log("in dashboard")
+  console.log("data: ", data)
+  console.log("error:", error)
+  console.log("loading:", loading)
 
 
 
